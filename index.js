5c329cbfd6f7ecbfed5bc9f596d3f68320c643c1
Yehwan Baek <baekokok98@gmail.com>
2023-03-15 20:46:09 +1100

    added code

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+let mainElement = document.getElementById("main");
+mainElement.remove();
+
+let newHeader = document.createElement("h1")
+newHeader.setAttribute("id","victory")
+newHeader.innerHTML = "Yehwan is the champion."
